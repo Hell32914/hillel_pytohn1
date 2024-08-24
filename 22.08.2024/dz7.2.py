19644b79b0fc1999e93873fc25e0f37f01041afa
Hell32914 <rybalka56@gmail.com>
2024-08-24 23:58:13 +0300

    com10

---
+++ 22.08.2024/dz7.2.py
@@ -0,0 +1,25 @@
+def correct_sentence(text):
+    # Перетворюємо текст на рядок + видаляю пробіли на початку та в кінці 
+    text = str(text).strip()
+    
+    # Якщо тексту немає 
+    if not text:
+        return ""
+    
+    # Щоб перша літера була велика
+    cor_text = text[0].upper() + text[1:]
+    
+    # Додаю крапку, якщо її немає
+    if not cor_text.endswith('.'):
+        cor_text += '.'
+    
+    return cor_text
+
+# Перевірка роботи
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+
+print('ОК')
---
+++ 22.08.2024/dz7.3.py
@@ -0,0 +1,21 @@
+def second_index(text, some_str):
+    # Знайти перше входження
+    first_index = text.find(some_str)
+    
+    # Якщо перше входження не знайдено - none
+    if first_index == -1:
+        return None
+    
+    # Шукати друге входження
+    second_index = text.find(some_str, first_index + len(some_str))
+    
+    # Якщо друге входження знайдено, повернути його індекс, інакше повернути None
+    return second_index if second_index != -1 else None
+
+# Перевірка роботи функції
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+
+print('ОК')
